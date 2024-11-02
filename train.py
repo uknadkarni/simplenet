9b5c9854a2ff7dc66360751af18b5f358b24c1cf
uknadkarni <10155870+uknadkarni@users.noreply.github.com>
2024-11-02 15:58:15 -0400

    Update train.py
    Added comments

--- train.py
+++ train.py
@@ -11,9 +11,19 @@ optimizer = optim.SGD(model.parameters, lr=0.01)
 num_epochs = 10
 for epoch in range(num_epochs):
     for inputs, labels in dataloader:
+        # Resets the gradients of all parameters (weights and biases) in the model to zero.
+        # Why it's necessary:
+        #   PyTorch accumulates gradients by default. If you don't zero the gradients, they will be accumulated with every backward pass.
+        #   This accumulation can lead to incorrect gradient calculations and updates.
         optimizer.zero_grad()
+        
         outputs = model(inputs)
         loss = criterion(outputs, labels)
+
+        # Backpropagation
         loss.backward()
+
+        # Updates the parameters of the model based on the computed gradients.
+        # Applies the optimization algorithm (in this case, SGD - Stochastic Gradient Descent) to update the parameters.
         optimizer.step()
         
